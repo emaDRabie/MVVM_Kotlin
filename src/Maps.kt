594f7a8aa35ef95990160e1bf3f6915ef18d4d16
emaDRabie <emadrabie046@gmail.com>
2025-04-07 17:26:35 +0200

    set updated!

--- src/Maps.kt
+++ src/Maps.kt
@@ -1,3 +1,15 @@
 fun main() {
-    
+    // Map: a collection of key-value pairs.
+    // Keys must be unique, but values can be duplicated.
+    val userPhones = mutableMapOf(
+        "Ahmed" to "01062500807",
+        "Emad" to "01115424444",
+        "Sara" to "01062050007"
+    )
+
+    println(userPhones)
+    userPhones["Emad"] = "020" // Updates Emad's number
+    userPhones["Laila"] = "01062050007" // Adds new entry with duplicate value
+    println(userPhones)
+
 }
